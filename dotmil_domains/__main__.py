c01ffc1c623f30e5211dda026ddc52da5f960a3e
Evan Sonderegger <evan.sonderegger@gmail.com>
2015-05-11 17:48:33 -0400

    lots of changes
    www is assumed to be root domain, Organization name is now guessed,
    domain names are sorted, and the air force site listings are scraped.

--- dotmil_domains/__main__.py
+++ dotmil_domains/__main__.py
@@ -16,25 +16,60 @@ def link_is_http(href):
         return True
 
 
-def domains_from_url(url):
-    domains_list = []
-    with open('mil.csv') as csvfile:
-        milreader = csv.reader(csvfile)
-        for row in milreader:
-            domains_list.append(row[0])
-    pages_to_scan = alphabet_pages(url)
+def sort_domains(domain_list):
+    reversed_domains = []
+    sorted_domains = []
+    for x in domain_list:
+        d = x.strip().split('.')
+        d.reverse()
+        reversed_domains.append(d)
+    reversed_domains.sort()
+    for y in reversed_domains:
+        y.reverse()
+        sorted_domains.append('.'.join(y))
+    return sorted_domains
+
+
+def domains_as_sorted_tuples():
+    domains_dict = {}
+    pages_to_scan = alphabet_pages()
+    pages_to_scan += air_force_site_pages()
     for page in pages_to_scan:
         soup = soup_from_url(page)
         if soup:
             all_links = soup.find_all('a')
             for link in all_links:
-                absolute = urlparse.urljoin(url, link.attrs['href'])
-                domain = urlparse.urlsplit(absolute)[1]
-                if (domain.endswith('.mil') and domain not in domains_list):
-                    domains_list.append(domain)
+                if ('href' in link.attrs) and link_is_http(link.attrs['href']):
+                    absolute = urlparse.urljoin(page, link.attrs['href'])
+                    domain = urlparse.urlsplit(absolute)[1]
+                    if (domain.split('.')[0] == 'www'):
+                        domain = '.'.join(domain.split('.')[1:])
+                    if domain.endswith('.mil') and domain not in domains_dict:
+                        if link.string:
+                            domains_dict[domain] = link.string.strip()
+                        else:
+                            domains_dict[domain] = "Unknown"
+                    elif (domain.endswith('.mil') and
+                          domains_dict[domain] == "Unknown"):
+                            if link.string:
+                                domains_dict[domain] = link.string.strip()
         else:
             logging.warning('we will need to re-scan: ' + page)
-    return domains_list
+
+    with open('mil.csv') as csvfile:
+        milreader = csv.reader(csvfile)
+        for row in milreader:
+            if (row[0] not in domains_dict):
+                domains_dict[row[0]] = "Unknown"
+
+    list_to_sort = []
+    for d in domains_dict:
+        list_to_sort.append(d)
+    sorted_list = sort_domains(list_to_sort)
+    domain_tuples = []
+    for d in sorted_list:
+        domain_tuples.append((d, domains_dict[d]))
+    return domain_tuples
 
 
 def soup_from_url(url):
@@ -53,26 +88,36 @@ def soup_from_url(url):
     return False
 
 
-def alphabet_pages(url):
+def alphabet_pages():
+    start_url = 'http://www.defense.gov/registeredsites/RegisteredSites.aspx'
     pages = []
-    soup = soup_from_url(url)
+    soup = soup_from_url(start_url)
     if soup:
         all_links = soup.find_all('a')
         for link in all_links:
             if (link.attrs['href'].startswith('RegisteredSites')):
-                absolute = urlparse.urljoin(url, link.attrs['href'], True)
+                absolute = urlparse.urljoin(start_url,
+                                            link.attrs['href'], True)
                 pages.append(absolute)
     else:
-        logging.warning('we will need to re-scan: ' + url)
+        logging.warning('we will need to re-scan: ' + start_url)
     return pages
 
 
+def air_force_site_pages():
+    af_list = ['http://www.af.mil/AFSites.aspx']
+    af_list.append('http://www.af.mil/AFSites.aspx?srBaseList=A')
+    return af_list
+
+
 def main():
-    start_url = 'http://www.defense.gov/registeredsites/RegisteredSites.aspx'
+    logging.basicConfig(level=logging.DEBUG)
     with open('dotmil-domains.csv', 'w') as csvfile:
         csv_writer = csv.writer(csvfile)
-        for domain in domains_from_url(start_url):
-            csv_writer.writerow([domain])
+        csv_writer.writerow(['Domain Name', 'Organization'])
+        domain_objects = domains_as_sorted_tuples()
+        for d in domain_objects:
+            csv_writer.writerow(d)
 
 
 if __name__ == "__main__":
